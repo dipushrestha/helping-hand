d54b4f0ed9b2540fb61ba3ba6c3e1bb254c12422
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2021-06-19 14:01:16 +0545

    HH: Mid update

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,7 +7,10 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using MudBlazor.Services;
+
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace helping_hand.Client
 {
@@ -19,8 +22,15 @@ namespace helping_hand.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddMudServices();
+            //builder.Services.AddMudServices();
+
+            builder.Services
+              .AddBlazorise(options =>
+              {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
 
             await builder.Build().RunAsync();
         }
