49c4553f1e1231c9e5703be2e3695f6d1a3fb396
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2021-08-10 12:09:23 +0545

    HH: Add notice entity

---
+++ Models/Notice.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace helping_hand.Models
+{
+    public class Notice
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Details { get; set; }
+    }
+}
